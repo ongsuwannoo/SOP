1000a582cfcae6becb71fea381cf09dc0bee0e3d
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-09-15 02:41:22 +0700

    delete

--- Main.java
+++
@@ -1,9 +0,0 @@
-package maven.appfirst;
-
-import org.springframework.boot.SpringApplication;
-
-public class Main {
-	public static void main(String[] args) {
-		SpringApplication.run(Car.class, args);
-	}
-}
