de9c9548ecc959470a72be603e85894e04a43c26
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-09-15 02:05:18 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package maven.appfirst;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Car.class, args);
+	}
+}
