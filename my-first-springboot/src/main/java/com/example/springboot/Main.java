c657a81f396b590474199419418c85d554dad5d4
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-09-15 02:37:10 +0700

    create workspace for Eclipse

---
+++ my-first-springboot/src/main/java/com/example/springboot/Main.java
@@ -0,0 +1,9 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Car.class, args);
+	}
+}
