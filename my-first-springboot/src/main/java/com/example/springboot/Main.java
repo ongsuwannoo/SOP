b3aa6a38db9598ff862d5f12fe5b493109b8f937
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-09-25 18:28:03 +0700

    microservice

--- my-first-springboot/src/main/java/com/example/springboot/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot;
-
-import org.springframework.boot.SpringApplication;
-
-public class Main {
-	public static void main(String[] args) {
-		SpringApplication.run(Car.class, args);
-	}
-}
