e330ef51224c8e00172294ff9154a8c5eb21e1f6
ongsuwannoo <ongsuwannoo@gmail.com>
2020-10-05 21:30:00 +0700

    edit

--- KMITLRegistrarsAPIs/svc-register/src/main/java/com/ong/springswagger/SpringswaggerApplication.java
+++ KMITLRegistrarsAPIs/svc-register/src/main/java/com/ong/springswagger/SpringswaggerApplication.java
@@ -2,11 +2,11 @@ package com.ong.springswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
+
 @EnableSwagger2
 @SpringBootApplication
 public class SpringswaggerApplication {
