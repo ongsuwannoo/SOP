90ab485462c6b8b93216d96cbc648731b97eb2d1
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-10-05 10:59:25 +0700

    update

---
+++ KMITLRegistrarsAPIs/svc-register/src/main/java/com/ong/springswagger/SpringswaggerApplication.java
@@ -0,0 +1,19 @@
+package com.ong.springswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableDiscoveryClient
+@EnableSwagger2
+@SpringBootApplication
+public class SpringswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringswaggerApplication.class, args);
+	}
+
+}
+
--- my-first-springboot/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into student
-values(10001,'Jenwit', 'E2001');
-
-insert into student
-values(10002,'Penjamrat', 'E2002');
